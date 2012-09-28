d15f2d829289a722ee464b18798014b97517b345
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-09-28 10:35:15 -0500

    Skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydracamp::Application.config.secret_token = 'd95bce30994e344cc4ee280cfcdb182ac1b30e395c5d9e72cec13d4dbb4c9b70f4046d9de63e2c190cf7a2e802e1075c74d4333f9aed8eb3eff3f228ee79d44a'

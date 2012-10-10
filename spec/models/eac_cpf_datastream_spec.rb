c12f2c8802c04a25f73d3231b5fb47096fcdb92b
Carolyn Cole <cam156@psu.edu>
2012-10-10 16:57:18 -0400

    Running tests

--- spec/models/eac_cpf_datastream_spec.rb
+++ spec/models/eac_cpf_datastream_spec.rb
@@ -3,19 +3,16 @@ require 'spec_helper'
 describe ZombieEacCpf do
    describe "with new datastream" do
      before do
-       @datastream = ZombieEacCpf.new
+       @datastream = ZombieEacCpf.new(nil,'EAC-CPF')
      end
      it "should have name" do
        test_attribute_xpath(@datastream, 'name', 'cpfDescription/oxns:identity/oxns:nameEntry/oxns:part')
      end
      it "should have graveyard" do
-       test_attribute_xpath(@datastream, 'graveyard', 'cpfDescription/places/place[localType='graveyard']/placeEntry')
+       test_attribute_xpath(@datastream, 'graveyard', "cpfDescription/places/place[localType='graveyard']/placeEntry")
      end
      it "should have nickname" do
        test_attribute_xpath(@datastream, 'nickname', 'cpfDescription/identity/nameEntryParallel')
-     end
-     it "should have active" do
-       test_attribute_xpath(@datastream, 'active', '/eac-cpf/cpfDescription/active')
      end
      #it "should have wins" do
      #  test_attribute_xpath(@datastream, 'wins', '/eac-cpf/cpfDescription/wins')
@@ -24,7 +21,7 @@ describe ZombieEacCpf do
      #  test_attribute_xpath(@datastream, 'losses', '/eac-cpf/cpfDescription/losses')
      #end
      it "should have weapon" do
-       test_attribute_xpath(@datastream, 'losses', '/eac-cpf/cpfDescription/weapon')
+       test_attribute_xpath(@datastream, 'weapon', '/eac-cpf/cpfDescription/weapon')
      end
      it "should have date_of_birth" do
        test_attribute_xpath(@datastream, 'date_of_birth', '/eac-cpf/cpfDescription/date_of_birth')
@@ -38,14 +35,14 @@ describe ZombieEacCpf do
    end
    describe "with existing datastream" do
      before do
-       @datastream = ZombieEacCpf.from_xml(File.new(File.join(File.dirname(__FILE__),'..'
-, 'spec','fixtures', "zombie_eac-cpf_authorized.xml")))
+       file = File.new(File.join(File.dirname(__FILE__),'..' ,'fixtures', "zombie_eac-cpf_authorized.xml"))
+       @datastream = ZombieEacCpf.from_xml(file)
      end
      it "should have name" do
        test_existing_attribute(@datastream, 'name', 'Cadell, Thomas')
      end
      it "should have graveyard" do
-       test_existing_attribute(@datastream, 'graveyard', 'Cadell, Thomas')
+       test_existing_attribute(@datastream, 'graveyard', 'Creepy Hallow')
      end
      it "should have nickname" do
        test_existing_attribute(@datastream, 'nickname', 'GRRAH')

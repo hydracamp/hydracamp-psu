3dd1ec1d8fb399c27119d5a7d86c496e26a96b29
WillHumphries <will.d.humphries@gmail.com>
2012-10-11 14:33:23 -0400

    Adding in facets with blacklight's div and our fields

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -51,14 +51,7 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    config.add_facet_field 'format', :label => 'Format'
-    config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
-    config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
-    config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
-    config.add_facet_field 'subject_geo_facet', :label => 'Region' 
-    config.add_facet_field 'subject_era_facet', :label => 'Era'  
-
+    config.add_facet_field 'weapon_t', :label => 'Weapon'
 
     config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
        :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
